cc1650ca645e15eedc6966c667fb9b75470bf088
Adam Beck <abeck@zendesk.com>
2016-10-19 23:21:08 +1100

    background for webhook server

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 npm install -g github-webhook
 mix ecto.create && mix ecto.migrate
 echo "running webhook server"
-github-webhook --config webhook.secret.json
+github-webhook --config webhook.secret.json &
