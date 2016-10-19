92c312bc9c8fa00745d549e1b5d862f4eb100dc0
Adam Beck <abeck@zendesk.com>
2016-10-19 21:30:07 +1100

    remove comments

--- mix.exs
+++ mix.exs
@@ -18,15 +18,6 @@ defmodule GmBot.Mixfile do
      mod: {GmBot, []}]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:simpler_slack, "~> 0.0.5"},
