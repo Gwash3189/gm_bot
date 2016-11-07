70c1084b8bf8925612deef75738f156b2d919728
Adam Beck <abeck@zendesk.com>
2016-11-07 22:20:23 +1100

    add routes for new what's

--- lib/router/what.ex
+++ lib/router/what.ex
@@ -3,7 +3,25 @@ defmodule GmBot.Routers.What do
 
   contains? [
     {"max hp", GmBot.Controllers.What.MaxHpController},
-    {"hp", GmBot.Controllers.What.CurrentHpController}
+    {"hp", GmBot.Controllers.What.CurrentHpController},
+
+    {"strength", GmBot.Controllers.What.StrengthController},
+    {"str", GmBot.Controllers.What.StrengthController},
+
+    {"dexterity", GmBot.Controllers.What.DexterityController},
+    {"dex", GmBot.Controllers.What.DexterityController},
+
+    {"constitution", GmBot.Controllers.What.ConstitutionController},
+    {"con", GmBot.Controllers.What.ConstitutionController},
+
+    {"intelligence", GmBot.Controllers.What.IntelligenceController},
+    {"int", GmBot.Controllers.What.IntelligenceController},
+
+    {"wisdom", GmBot.Controllers.What.WisdomController},
+    {"wis", GmBot.Controllers.What.WisdomController},
+
+    {"charisma", GmBot.Controllers.What.CharismaController},
+    {"cha", GmBot.Controllers.What.CharismaController}
   ]
 
   default "I'm don't know what that is. Try asking for help."
